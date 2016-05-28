93cac1b89f9e4bae9302b29e33210ea64ebacc9e
Chris Mackay <chkbookapp@gmail.com>
2016-05-28 17:04:58 -0400

    Added All CheckbookMessage Files

---
+++ CheckbookMessage/CheckbookMessage.Designer.vb
@@ -0,0 +1,135 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CheckbookMessage
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlWhiteSpace = New System.Windows.Forms.Panel()
+        Me.lblBodyMessage = New System.Windows.Forms.Label()
+        Me.lblHeaderMessage = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.OK_Button = New System.Windows.Forms.Button()
+        Me.Cancel_Button = New System.Windows.Forms.Button()
+        Me.pnlWhiteSpace.SuspendLayout()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlWhiteSpace
+        '
+        Me.pnlWhiteSpace.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pnlWhiteSpace.BackColor = System.Drawing.Color.White
+        Me.pnlWhiteSpace.Controls.Add(Me.lblBodyMessage)
+        Me.pnlWhiteSpace.Controls.Add(Me.lblHeaderMessage)
+        Me.pnlWhiteSpace.Location = New System.Drawing.Point(0, 0)
+        Me.pnlWhiteSpace.Name = "pnlWhiteSpace"
+        Me.pnlWhiteSpace.Size = New System.Drawing.Size(383, 79)
+        Me.pnlWhiteSpace.TabIndex = 2
+        '
+        'lblBodyMessage
+        '
+        Me.lblBodyMessage.AutoSize = True
+        Me.lblBodyMessage.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblBodyMessage.Location = New System.Drawing.Point(12, 48)
+        Me.lblBodyMessage.MaximumSize = New System.Drawing.Size(353, 0)
+        Me.lblBodyMessage.Name = "lblBodyMessage"
+        Me.lblBodyMessage.Size = New System.Drawing.Size(42, 13)
+        Me.lblBodyMessage.TabIndex = 3
+        Me.lblBodyMessage.Text = "Sample"
+        '
+        'lblHeaderMessage
+        '
+        Me.lblHeaderMessage.AutoSize = True
+        Me.lblHeaderMessage.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeaderMessage.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(51, Byte), Integer), CType(CType(188, Byte), Integer))
+        Me.lblHeaderMessage.Location = New System.Drawing.Point(12, 9)
+        Me.lblHeaderMessage.Margin = New System.Windows.Forms.Padding(5, 0, 3, 18)
+        Me.lblHeaderMessage.MaximumSize = New System.Drawing.Size(353, 0)
+        Me.lblHeaderMessage.Name = "lblHeaderMessage"
+        Me.lblHeaderMessage.Size = New System.Drawing.Size(62, 21)
+        Me.lblHeaderMessage.TabIndex = 2
+        Me.lblHeaderMessage.Text = "Sample"
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(232, 83)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
+        Me.TableLayoutPanel1.TabIndex = 3
+        '
+        'OK_Button
+        '
+        Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.OK_Button.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.OK_Button.Location = New System.Drawing.Point(3, 3)
+        Me.OK_Button.Name = "OK_Button"
+        Me.OK_Button.Size = New System.Drawing.Size(67, 23)
+        Me.OK_Button.TabIndex = 0
+        Me.OK_Button.Text = "OK"
+        '
+        'Cancel_Button
+        '
+        Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
+        Me.Cancel_Button.Name = "Cancel_Button"
+        Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
+        Me.Cancel_Button.TabIndex = 1
+        Me.Cancel_Button.Text = "Cancel"
+        '
+        'CheckbookMessage
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(383, 118)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.pnlWhiteSpace)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(360, 157)
+        Me.Name = "CheckbookMessage"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+        Me.Text = "Checkbook"
+        Me.pnlWhiteSpace.ResumeLayout(False)
+        Me.pnlWhiteSpace.PerformLayout()
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents pnlWhiteSpace As System.Windows.Forms.Panel
+    Friend WithEvents lblHeaderMessage As System.Windows.Forms.Label
+    Friend WithEvents lblBodyMessage As System.Windows.Forms.Label
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents OK_Button As System.Windows.Forms.Button
+    Friend WithEvents Cancel_Button As System.Windows.Forms.Button
+
+End Class
---
+++ CheckbookMessage/CheckbookMessage.vb
@@ -0,0 +1,102 @@
+﻿Imports System.Windows.Forms
+
+Public Class CheckbookMessage
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Me.Dispose()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Dispose()
+    End Sub
+
+    Private Sub frmDialog_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim headerMessageBottom As Integer = lblHeaderMessage.Location.Y + lblHeaderMessage.Height 'GETS THE Y LOCATION OF THE BOTTOM
+        Dim bodyMessageBottom As Integer = lblBodyMessage.Location.Y + Me.lblBodyMessage.Height 'GETS THE Y LOCATION OF THE BOTTOM
+
+        Dim bodyMessageTop As Integer = headerMessageBottom + 18 'OFFSETS THE TOP OF THE BODY FROM THE BOTTOM OF THE HEADER
+
+        lblBodyMessage.Location = New Drawing.Point(Me.lblBodyMessage.Location.X, bodyMessageTop) 'SETS NEW BODY TOP
+
+        If lblBodyMessage.Text = String.Empty Then
+
+            Me.Height += (lblHeaderMessage.Location.Y + lblHeaderMessage.Height) - 50 'IF ONLY THE HEADER TEXT IS SHOWN THEN MAKE THE FORM A LITTLE SMALLER
+
+        Else
+
+            Me.Height += (lblBodyMessage.Location.Y + lblBodyMessage.Height) - 60
+
+        End If
+
+        centerFormCenterScreen(Me)
+
+    End Sub
+
+    Public Function ShowMessage(ByVal HeaderMessage As String, ByVal Buttons As MsgButtons, Optional ByVal BodyMessage As String = "", Optional ByVal MessageSound As Media.SystemSound = Nothing)
+
+        Me.Height = 157
+        Me.lblHeaderMessage.Text = HeaderMessage
+        Me.lblBodyMessage.Text = BodyMessage
+
+        'MSG BUTTONS
+        Select Case Buttons
+
+            Case MsgButtons.OK
+
+                OK_Button.Visible = False
+
+                Cancel_Button.DialogResult = Windows.Forms.DialogResult.OK
+                Cancel_Button.Text = "OK"
+
+            Case MsgButtons.OKCancel
+
+                OK_Button.DialogResult = Windows.Forms.DialogResult.OK
+                Cancel_Button.DialogResult = Windows.Forms.DialogResult.Cancel
+
+                OK_Button.Text = "OK"
+                Cancel_Button.Text = "Cancel"
+
+            Case MsgButtons.YesNo
+
+                OK_Button.DialogResult = Windows.Forms.DialogResult.Yes
+                Cancel_Button.DialogResult = Windows.Forms.DialogResult.No
+
+                OK_Button.Text = "Yes"
+                Cancel_Button.Text = "No"
+
+        End Select
+
+        If Not IsNothing(MessageSound) Then
+
+            MessageSound.Play()
+
+        End If
+
+        Me.ShowDialog()
+
+        Dim dialogResult As System.Windows.Forms.DialogResult
+        dialogResult = Me.DialogResult
+
+        Return dialogResult
+    End Function
+
+    Public Enum MsgButtons
+
+        OK = 0
+        OKCancel = 1
+        YesNo = 2
+
+    End Enum
+
+    Sub centerFormCenterScreen(ByVal oForm As Object)
+
+        Dim currentArea = Screen.FromControl(oForm).WorkingArea
+        oForm.Top = currentArea.Top + CInt((currentArea.Height / 2) - (oForm.Height / 2))
+        oForm.Left = currentArea.Left + CInt((currentArea.Width / 2) - (oForm.Width / 2))
+
+    End Sub
+
+End Class
